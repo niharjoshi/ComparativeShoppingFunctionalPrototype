a0ed0c241bef672b8aac6175939872dec813d9fe
Nihar Joshi <nsj0596@gmail.com>
2022-10-08 23:59:02 -0500

    Updates

--- precache-manifest.cedfb3a138ac1a4d62cce251906c1d17.js
+++ precache-manifest.cedfb3a138ac1a4d62cce251906c1d17.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "dcee7bc580216d7146d7",
+    "revision": "655c95a0d24d61c7a62e",
     "url": "/ComparativeShoppingFunctionalPrototype/static/css/main.67485e74.chunk.css"
   },
   {
-    "revision": "dcee7bc580216d7146d7",
-    "url": "/ComparativeShoppingFunctionalPrototype/static/js/main.dcee7bc5.chunk.js"
+    "revision": "655c95a0d24d61c7a62e",
+    "url": "/ComparativeShoppingFunctionalPrototype/static/js/main.655c95a0.chunk.js"
   },
   {
     "revision": "1b588285ee68e358872f",
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/ComparativeShoppingFunctionalPrototype/static/js/runtime~main.43016c74.js"
   },
   {
-    "revision": "aa2737dbd24eee160db33181912eecaa",
+    "revision": "d4dcb59809f452ee809de7f721a73eb7",
     "url": "/ComparativeShoppingFunctionalPrototype/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ComparativeShoppingFunctionalPrototype/precache-manifest.c0eb3dc8ff450520a084aa12b5c34157.js"
+  "/ComparativeShoppingFunctionalPrototype/precache-manifest.cedfb3a138ac1a4d62cce251906c1d17.js"
 );
 
 workbox.clientsClaim();
