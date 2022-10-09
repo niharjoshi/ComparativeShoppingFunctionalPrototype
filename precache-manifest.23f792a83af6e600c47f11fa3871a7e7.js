11ade7ef5f546c9d2004723e9abe9d8af00c9a58
Nihar Joshi <nsj0596@gmail.com>
2022-10-08 20:04:01 -0500

    Updates

---
+++ precache-manifest.23f792a83af6e600c47f11fa3871a7e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4ddf1446fee84cdd555",
+    "url": "/static/css/main.67485e74.chunk.css"
+  },
+  {
+    "revision": "e4ddf1446fee84cdd555",
+    "url": "/static/js/main.e4ddf144.chunk.js"
+  },
+  {
+    "revision": "f6f591f1272ed63149d8",
+    "url": "/static/css/1.ded887f7.chunk.css"
+  },
+  {
+    "revision": "f6f591f1272ed63149d8",
+    "url": "/static/js/1.f6f591f1.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a87786c96efee139bdde3439fdf67ac0",
+    "url": "/index.html"
+  }
+];
